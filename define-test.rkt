56ea267e535982f162bdeacf1d288aa602a6bbbc
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-03-20 20:56:53 +0200

    Added hijack version, complete hack

--- define-test.rkt
+++ define-test.rkt
@@ -2,6 +2,15 @@
 
 (provide define/tc
          define/tc/module
+         define/tc/hijack
+         define/tc/lift
+
+         %test-list%
+         
+         (except-out (all-from-out racket/base)
+                     #%module-begin)
+         (rename-out [module-begin #%module-begin])
+
          (all-from-out rackunit
                        racket/contract))
 
@@ -11,6 +20,21 @@
                      racket/contract)
          rackunit
          racket/contract)
+
+(define %test-list% (list))
+
+(define (add-test function input output)
+  (set! %test-list% (append %test-list%
+                            (list (list function input output)))))
+
+(define (%run-test% t)
+  (check-equal? (apply (car t) (cadr t)) (caddr t)))
+
+(define-syntax-rule (module-begin e ...)
+  (#%module-begin
+   e ...
+   (module+ test
+     (for-each %run-test% %test-list%))))
 
 ;;; Macro for defining with contract and tests at the same time
 ;;; Usage is illustrated below.
@@ -60,6 +84,37 @@
            body ...)
          (module+ test
            (check-equal? (func-name test-input) desired-result) ...))]))
+
+(define-syntax (define/tc/hijack stx)
+  (syntax-parse stx
+    [(tc (func-name:id arg:id ...)
+         contract-spec
+         ([(test-input:expr ...) desired-result:expr] ...)
+         body:expr ...)
+     #'(begin
+         (define/contract (func-name arg ...)
+           contract-spec
+           body ...)
+         (for-each (lambda (t)
+                     (add-test func-name (car t) (cdr t)))
+                   (list (cons (list test-input ...)
+                               desired-result)
+                         ...)))]))
+
+(define-syntax (define/tc/lift stx)
+  (syntax-parse stx
+    [(tc (func-name:id arg:id ...)
+         contract-spec
+         ([(test-input:expr ...) desired-result:expr] ...)
+         body:expr ...)
+     (syntax-local-lift-module-end-declaration
+      #'(module+ test
+          (check-equal? (func-name test-input ...) desired-result)
+          ...))
+     #'(begin
+         (define/contract (func-name arg ...)
+           contract-spec
+           body ...))]))
            
 (module+ main
   (define/tc (square x)
