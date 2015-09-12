74fe9f468735047ee521ae32a5703716f2e466f3
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-09-12 13:58:13 +0300

    Made module variant only variant of `define/tc`
    Adds tests to `test` module

--- define-test.rkt
+++ define-test.rkt
@@ -1,97 +1,55 @@
 #lang racket/base
 
-(provide define/tc
-		 define/tc/module
-		 define/tc/lift
-
-		 (all-from-out rackunit
-					   racket/contract))
+(provide (all-from-out rackunit
+                       racket/contract))
 
 (require (for-syntax racket/base
-					 racket/syntax
-					 syntax/parse
-					 racket/contract)
-		 rackunit
-		 racket/contract)
+                     racket/syntax
+                     syntax/parse
+                     racket/contract)
+         rackunit
+         racket/contract)
 
 ;;; Macro for defining with contract and tests at the same time
 ;;; Usage is illustrated below.
+(provide define/tc)
 (define-syntax (define/tc stx)
   (syntax-parse stx
-	[(tc (func-name:id arg ...)
-		 contract-spec
-		 ([(test-input:expr ...) desired-result:expr] ...)
-		 body:expr ...)
-	 #'(begin
-		 (define/contract (func-name arg ...)
-		   contract-spec
-		   body ...)
-		 (check-equal? (func-name test-input ...) desired-result)
-		 ...)]
-	[(tc (func-name:id arg ...)
-		 contract-spec
-		 ([test-input:expr desired-result:expr] ...)
-		 body:expr ...)
-	 #'(begin
-		 (define/contract (func-name arg ...)
-		   contract-spec
-		   body ...)
-		 (check-equal? (func-name test-input) desired-result)
-		 ...)]))
+    [(tc (func-name:id arg:id ...)
+         contract-spec
+         ([(test-input:expr ...) desired-result:expr] ...)
+         body:expr ...)
+     #'(begin
+         (define/contract (func-name arg ...)
+           contract-spec
+           body ...)
+         (module+ test
+           (check-equal? (func-name test-input ...) desired-result)
+           ...))]
+    [(tc (func-name:id arg:id ...)
+         contract-spec
+         ([test-input:expr desired-result:expr] ...)
+         body:expr ...)
+     #'(begin
+         (define/contract (func-name arg ...)
+           contract-spec
+           body ...)
+         (module+ test
+           (check-equal? (func-name test-input) desired-result) ...))]))
 
-(define-syntax (define/tc/module stx)
-  (syntax-parse stx
-	[(tc (func-name:id arg:id ...)
-		 contract-spec
-		 ([(test-input:expr ...) desired-result:expr] ...)
-		 body:expr ...)
-	 #'(begin
-		 (define/contract (func-name arg ...)
-		   contract-spec
-		   body ...)
-		 (module+ test
-		   (check-equal? (func-name test-input ...) desired-result)
-		   ...))]
-	[(tc (func-name:id arg:id ...)
-		 contract-spec
-		 ([test-input:expr desired-result:expr] ...)
-		 body:expr ...)
-	 #'(begin
-		 (define/contract (func-name arg ...)
-		   contract-spec
-		   body ...)
-		 (module+ test
-		   (check-equal? (func-name test-input) desired-result) ...))]))
+(define/tc (square x)
+  (integer? . -> . integer?)
 
-(define-syntax (define/tc/lift stx)
-  (syntax-parse stx
-	[(tc (func-name:id arg:id ...)
-		 contract-spec
-		 ([(test-input:expr ...) desired-result:expr] ...)
-		 body:expr ...)
-	 (syntax-local-lift-module-end-declaration
-	   #'(module+ test
-		   (check-equal? (func-name test-input ...) desired-result)
-		   ...))
-	 #'(begin
-		 (define/contract (func-name arg ...)
-		   contract-spec
-		   body ...))]))
+  ([2 4]
+   [3 9]
+   [5 25])
 
-(module+ main
-  (define/tc (square x)
-	(integer? . -> . integer?)
+  (expt x 2))
 
-	([2 4]
-	 [3 9]
-	 [5 25])
+(define/tc (square2&add x y)
+  (integer? integer? . -> . integer?)
 
-	(expt x 2))
+  ([(2 3) 13]
+   [(1 2) 5])
 
-  (define/tc (square2&add x y)
-	(integer? integer? . -> . integer?)
-
-	([(2 3) 13]
-	 [(1 2) 5])
-
-	(+ (expt x 2) (expt y 2))))
+  (+ (expt x 2) (expt y 2)))
