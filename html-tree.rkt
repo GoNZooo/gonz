c7a3c73b2556603ad35a15e5a175dad193d31b4f
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-09-12 13:49:10 +0300

    Add examples in main module

--- html-tree.rkt
+++ html-tree.rkt
@@ -1,31 +1,42 @@
 #lang racket/base
 
 (require racket/match
-		 racket/port
-		 net/url
-		 xml
-		 html)
-
-(provide url->xexprs
-		 find*)
+         xml
+         html
+         net/url)
 
 (permissive-xexprs #t)
 
+;; (-> string? xexpr?)
+(provide url->xexprs)
 (define (url->xexprs base-url)
   (map xml->xexpr
-	   (call/input-url (string->url base-url)
-					   get-pure-port
-					   read-html-as-xml)))
+       (call/input-url (string->url base-url)
+                       get-pure-port
+                       read-html-as-xml)))
 
+;; (-> xexpr? symbol? xexpr?)
+(provide find*)
 (define (find* root element-type)
   (match root
-	['() '()]
-	[(list (list sub-exprs ...) exprs ...)
-	 (append (find* sub-exprs element-type)
-			 (find* exprs element-type))]
-	[(list expr exprs ...)
-	 #:when (equal? element-type expr)
-	 (append `((,expr ,exprs)) (find* exprs element-type))]
-	[(list expr exprs ...)
-	 (find* exprs element-type)]))
+    ['() '()]
+    [(list (list sub-exprs ...) exprs ...)
+     (append (find* sub-exprs element-type)
+             (find* exprs element-type))]
+    [(list expr exprs ...)
+     #:when (equal? element-type expr)
+     (append `((,expr ,exprs)) (find* exprs element-type))]
+    [(list expr exprs ...)
+     (find* exprs element-type)]))
 
+(module+ main
+  (define page-xexpr (url->xexprs "http://severnatazvezda.com/text/"))
+  ;; Finds all links on a page
+  (find* page-xexpr
+         'a)
+  ;; Finds all divs on a page
+  (find* page-xexpr
+         'div)
+  ;; Finds all hrefs on a page
+  (find* page-xexpr
+         'href))
