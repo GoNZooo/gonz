86bbdfb8df814e7ffc75b8eda94d665c870c1e99
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-09-12 13:59:25 +0300

    Fix indentation in gui-helpers.rkt

--- gui-helpers.rkt
+++ gui-helpers.rkt
@@ -16,75 +16,75 @@
 
 (define-syntax (btn stx)
   (syntax-case stx ()
-	[(_ sname sparent slabel scallback opt ...)
-	 (with-syntax ([name (format-id stx
-									"~a"
-									(syntax->datum #'sname))])
-	   #'(define name (new button% [parent sparent]
-						   [label slabel]
-						   [callback scallback]
-						   opt ...)))]))
+    [(_ sname sparent slabel scallback opt ...)
+     (with-syntax ([name (format-id stx
+                                    "~a"
+                                    (syntax->datum #'sname))])
+       #'(define name (new button% [parent sparent]
+                           [label slabel]
+                           [callback scallback]
+                           opt ...)))]))
 
 (define-syntax (tfield stx)
   (syntax-case stx ()
-	[(_ sname sparent slabel scallback)
-	 (with-syntax ([name (format-id stx
-									"~a"
-									(syntax->datum #'sname))])
-	   #'(define name (new text-field% [parent sparent]
-						   [label slabel]
-						   [callback scallback]
-						   [style '(single vertical-label)])))]
-	[(_ sname sparent slabel scallback opt ...)
-	 (with-syntax ([name (format-id stx
-									"~a"
-									(syntax->datum #'sname))])
-	   #'(define name (new text-field% [parent sparent]
-						   [label slabel]
-						   [callback scallback]
-						   opt ...)))]))
+    [(_ sname sparent slabel scallback)
+     (with-syntax ([name (format-id stx
+                                    "~a"
+                                    (syntax->datum #'sname))])
+       #'(define name (new text-field% [parent sparent]
+                           [label slabel]
+                           [callback scallback]
+                           [style '(single vertical-label)])))]
+    [(_ sname sparent slabel scallback opt ...)
+     (with-syntax ([name (format-id stx
+                                    "~a"
+                                    (syntax->datum #'sname))])
+       #'(define name (new text-field% [parent sparent]
+                           [label slabel]
+                           [callback scallback]
+                           opt ...)))]))
 
 (define-syntax (msg stx)
   (syntax-case stx ()
-	[(_ sname sparent slabel opt ...)
-	 (with-syntax ([name-sym (format-id stx
-										"~a"
-										(syntax->datum #'sname))])
-	   #'(define name-sym (new message% [parent sparent]
-							   [label slabel]
-							   opt ...)))]))
+    [(_ sname sparent slabel opt ...)
+     (with-syntax ([name-sym (format-id stx
+                                        "~a"
+                                        (syntax->datum #'sname))])
+       #'(define name-sym (new message% [parent sparent]
+                               [label slabel]
+                               opt ...)))]))
 
 ; hpanel macro with options
 (define-syntax (hpanel stx)
   (syntax-case stx ()
-	[(_ sname sparent opt ...)
-	 (with-syntax ([name-sym (format-id stx
-										"~a"
-										(syntax->datum #'sname))])
-	   #'(define name-sym (new horizontal-panel% [parent sparent]
-							   opt ...)))]))
+    [(_ sname sparent opt ...)
+     (with-syntax ([name-sym (format-id stx
+                                        "~a"
+                                        (syntax->datum #'sname))])
+       #'(define name-sym (new horizontal-panel% [parent sparent]
+                               opt ...)))]))
 
 (define-syntax (vpanel stx)
   (syntax-case stx ()
-	[(_ sname sparent opt ...)
-	 (with-syntax ([name-sym (format-id stx
-										"~a"
-										(syntax->datum #'sname))])
-	   #'(define name-sym (new vertical-panel% [parent sparent]
-							   opt ...)))]))
+    [(_ sname sparent opt ...)
+     (with-syntax ([name-sym (format-id stx
+                                        "~a"
+                                        (syntax->datum #'sname))])
+       #'(define name-sym (new vertical-panel% [parent sparent]
+                               opt ...)))]))
 
 (define-syntax (lbox stx)
   (syntax-case stx ()
-	[(_ sname sparent slabel sdata scallback opt ...)
-	 (with-syntax ([name-sym (format-id stx
-										"~a"
-										(syntax->datum #'sname))])
-	   #'(begin
-		   (define name-sym (new list-box% [parent sparent]
-								 [label slabel]
-								 [choices '()]
-								 [callback scallback]
-								 opt ...))))]))
+    [(_ sname sparent slabel sdata scallback opt ...)
+     (with-syntax ([name-sym (format-id stx
+                                        "~a"
+                                        (syntax->datum #'sname))])
+       #'(begin
+           (define name-sym (new list-box% [parent sparent]
+                                 [label slabel]
+                                 [choices '()]
+                                 [callback scallback]
+                                 opt ...))))]))
 
 (define (event-equal? event type)
   (equal? (send event get-event-type) type))
