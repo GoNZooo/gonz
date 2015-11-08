18804ae1a336ba882a94ab0e37695ba584469fde
Rickard Andersson <gonz@severnatazvezda.com>
2015-11-08 12:08:17 +0200

    Add fail-expr to specify what to return when key's not present

--- nested-hash.rkt
+++ nested-hash.rkt
@@ -8,18 +8,25 @@
 
 ; (hash-ref h 'foo 'bar) => (hash-ref (hash-ref h 'foo) 'bar)
 
-(define (chain-keys h keys)
+(define (chain-keys h keys [fail 'no-fail])
   (if (null? keys)
     h
-    (chain-keys (hash-ref h (car keys))
-                (cdr keys))))
+    (if (equal? fail 'no-fail)
+      (chain-keys (hash-ref h (car keys))
+                  (cdr keys)
+                  fail)
+      (chain-keys (hash-ref h (car keys) fail)
+                  (cdr keys)
+                  fail))))
 
 (define-syntax (hash-ref* stx)
   (syntax-parse stx
-    [(_ hash-object key ...)
-     #'(chain-keys hash-object (list key ...))]))
+    [(_ hash-object key:expr ...)
+     #`(chain-keys hash-object (list key ...))]
+    [(_ hash-object key:expr ... #:fail? fail-expr:expr)
+     #`(chain-keys hash-object (list key ...) fail-expr)]))
 
 (module+ main
   (define h '#hash((foo . #hash((bar . 10)))))
   
-  (hash-ref* h 'foo 'bar))
+  (hash-ref* h 'foo 'baar #:fail? ""))
